743f6fe2cfb005933d986f9ee7614c77b69332fc
Matan Vardi <matanvardi@matanvr.local>
2014-05-25 04:16:26 -0700

    merged sort of

--- src/com/myteam/thisorthat/CommentsActivity.java
+++ src/com/myteam/thisorthat/CommentsActivity.java
@@ -1,6 +1,10 @@
 package com.myteam.thisorthat;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
 import android.view.View.OnClickListener;
 
 import com.parse.FindCallback;
@@ -8,65 +12,118 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 import com.squareup.picasso.Picasso;
+
 public class CommentsActivity extends Activity {
 	Button addComment;
-	TextView postMsg; 
+	TextView comment; 
 	String postId;
 	String userId;
+	String userName;
+	
+	
+	SwipeRefreshLayout mSwipeRefreshLayout;
+	ArrayAdapter<String> adapter;
+	protected OnRefreshListener mOnRefreshListener = new OnRefreshListener() {
+		@Override
+		public void onRefresh() {
+			 
+				refresh();
+		
+		}
+	};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_comments);
 		postId = getIntent().getStringExtra("postId");
 		userId = getIntent().getStringExtra("userId");
+		userName = getIntent().getStringExtra("userName");
+		comment = (TextView)findViewById(R.id.commentInputText);
+		mSwipeRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.swipeRefreshLayout);
+		mSwipeRefreshLayout.setOnRefreshListener(mOnRefreshListener);
+		mSwipeRefreshLayout.setColorScheme(
+				R.color.swipeRefresh1,
+				R.color.swipeRefresh2,
+				R.color.swipeRefresh3,
+				R.color.swipeRefresh4);
+		refresh();
+		
+		
 		addComment = (Button)findViewById(R.id.addComment);
 		addComment.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                	Intent intent = new Intent(CommentsActivity.this, AddCommentActivity.class);
-					intent.putExtra("postId", postId);
-					intent.putExtra("userId", postId);
-					startActivity(intent);
+                	
+                	ParseObject parse = new ParseObject("Comment");
+            		parse.put("postId", postId);
+            		parse.put("userId", userId);
+            		parse.put("commentText", comment.getText().toString());
+            		parse.put("username", userName);
+            		parse.saveInBackground();
+            		refresh();
+            		comment.setText("");
+            		comment.clearFocus();
+            		
+            		InputMethodManager imm = (InputMethodManager) view.getContext()
+            			    .getSystemService(Context.INPUT_METHOD_SERVICE);
+            			 imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
                 }
         });
-
-		ParseQuery<ParseObject> query = ParseQuery.getQuery("Comment");
-		query.whereEqualTo("postId", postId);
 		
-		query.findInBackground(new FindCallback<ParseObject>() {
-			@Override
-			public void done(List<ParseObject> commentList, com.parse.ParseException e) {
-				String comments = "";
-				for(ParseObject comment : commentList){
-					comments += comment.getString("commentText")+"\n";
-				}
-				postMsg = (TextView)findViewById(R.id.postMessage);
-				postMsg.setText(comments);
-				
-			}
-		}); 
 		
 		
 	}
-	
-	
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.comments, menu);
 		return true;
+	}
+	
+	private void populateListView(ArrayList<String> list){
+		adapter = new ArrayAdapter<String>(this, R.layout.comment_item, list);
+		ListView commentListV = (ListView) findViewById(R.id.commentListView);
+		commentListV.setAdapter(adapter);
+		
+	}
+	private void refresh(){
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("Comment");
+		query.whereEqualTo("postId", postId);
+
+		
+		query.findInBackground(new FindCallback<ParseObject>() {
+			@Override
+			public void done(List<ParseObject> commentList, com.parse.ParseException e) {
+				ArrayList<String> list = new ArrayList<String>();
+				for(ParseObject comment : commentList){
+					list.add(comment.getString("username")+": "+comment.getString("commentText"));
+				}
+				populateListView(list);
+				
+			}
+		});
+		
+		if (mSwipeRefreshLayout.isRefreshing()) {
+			mSwipeRefreshLayout.setRefreshing(false);
+		}
 	}
 
 }
